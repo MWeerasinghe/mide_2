e90a836ec2475dc33357da84ee4ddb739f985dfc
ishanchamika <ishanchami9@gmail.com>
2025-01-18 14:29:19 +0530

    change databasee usernames and passwords for local postgresql

--- backend/models/sequelize.js
+++ backend/models/sequelize.js
@@ -1,32 +1,16 @@
-// const { Sequelize } = require('sequelize');
-
-// // Use the DATABASE_URL environment variable
-// const databaseUrl = process.env.DATABASE_URL;
-
-// if (!databaseUrl) {
-//   throw new Error('DATABASE_URL environment variable is not set.');
-// }
-
-// const sequelize = new Sequelize(databaseUrl, {
-//   dialect: 'postgres',
-//   logging: false, // Disable logging if needed
-// });
-
-// module.exports = sequelize;
-
-
-
-
-
 const { Sequelize } = require('sequelize');
 
-// Create a Sequelize instance with PostgreSQL configuration
-const sequelize = new Sequelize('yourdb', 'vajiraramaya', '1001', {
-    host: 'localhost',
-    dialect: 'postgres', // Specifies PostgreSQL
-    port: 5432,          // Default PostgreSQL port
-    logging: false,      // Set to true to enable query logging
+// Use the DATABASE_URL environment variable
+const databaseUrl = "postgres://postgres:1001@localhost:5432/vajira";
+// const databaseUrl = "postgres://zeus:yourpassword@db:5432/yourdb";
+
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL environment variable is not set.');
+}
+
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
+  logging: false, // Disable logging if needed
 });
 
-// Export the Sequelize instance
-module.exports = { sequelize };
+module.exports = sequelize;
